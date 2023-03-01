46eb4ce67d3a031b25a9c0db7ffcea461684c20a
José Lucas <josluc2016@gmail.com>
2023-03-01 18:04:39 -0300

    upload

---
+++ funcoes/basicas/basicas.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func f1() {
+	fmt.Println("F1")
+}
+
+func f2(p1 string, p2 string) {
+	fmt.Printf("F2: %s %s\n", p1, p2)
+}
+
+func f3() string {
+	return "F3"
+}
+
+func f4(p1, p2 string) string {
+	return fmt.Sprintf("F4: %s %s", p1, p2)
+}
+
+func f5() (string, string) {
+	return "R1", "R2"
+}
+
+func main() {
+	f1()
+	f2("José", "Lucas")
+	fmt.Println(f3())
+	fmt.Println(f4("Kendrick", "Lamar"))
+	r51, _ := f5()
+	fmt.Println("F5:", r51)
+}
